bee25655a563b94d39f7e91676295a1d6fae35c7
hubiruchi <yooonatan@gmail.com>
2017-01-27 16:36:24 +0200

    made signup possible and added to menu

--- app.py
+++ app.py
@@ -8,51 +8,72 @@ from teams import Team
 from games import Game
 from users import User
 
-
 app = Flask(__name__)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
+
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.get(user_id)
 
+
 @app.route("/")
 def index():
     return render_template('index.html', name="Admin")
+
 
 # route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+        print request.form.get("formtype")
+        if request.form["formtype"] == 'login':
+            print"login"
         else:
-            return redirect(url_for('index'))
+            signup()
     return render_template('login.html', error=error)
-    
+
+
+def signup():
+    usr = User(request.form['username'], request.form['password'], request.form['email'])
+    db_session.add(usr)
+    print usr
+    return redirect(url_for('games'))
+
+
 @app.route("/teams")
 def teams():
     teams = Team.query.all()
     return render_template("teams.html", teams=teams)
 
+
+@app.route("/users")
+def users():
+    usr = User.query.all()
+    return render_template("users.html", users=usr)
+
+
 @app.route("/team/<teamnumber>")
 def team_page(teamnumber):
     team = Team.query.filter_by(number='teamnumber').first()
     return render_template('team_page.html', team=team)
-	
+
+
 @app.route("/games")
 def games():
     games = Game.query.all()
     return render_template('games.html', games=games)
-    
+
+
 @app.route("/game/<gamenumber>")
 def game_page(gamenumber):
     game = Game.query.filter_by(number='gamenumber').first()
